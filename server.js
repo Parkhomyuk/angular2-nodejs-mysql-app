9c56f3c63f4f5574f36500ed173fe0e5e7c8dea9
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-17 23:12:50 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+/*
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var port = 3000;
+
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'client/src')));
+
+// Body Parser MW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.listen(port, function(){
+    console.log('Server started on port '+port);
+});
+
+
+*/
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+app.use('/',express.static(__dirname+'/client/dist'));
+// Body Parser MW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.get('/api', function (req, res) {
+    res.sendFile(path.join(__dirname+'/client/dist','index.html'))
+});
+
+/*app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});*/
+
+
+
+app.listen(3000, function () {
+    console.log('Example listening on port 3000!');
+});
+
+module.exports = app;
