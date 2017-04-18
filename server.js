7f1de031820aaf264c1122379abd0088b6c62d53
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-18 20:59:53 +0300

    update+public+return+12

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 /*app.use('/', index);
 app.use('/api', tasks);*/
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/dist/index.html'));*/
+    res.sendFile(path.join(__dirname+'/public/dist/index.html'));
      /*res.send('Hi!');*/
 
 });
