23de9ff20e21f89bf9e974e1100b9440881d60bf
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-19 11:50:28 +0300

    update+public+return+13

--- server.js
+++ server.js
@@ -38,10 +38,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
-app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname+'/public/dist'));
-/!*app.use('/',express.static(__dirname+'/public/dist'));*!/
-/!*app.use('/',express.static(__dirname+'/client/dist'));*!/
+
+app.use('/',express.static(__dirname+'/client/dist'));
 // Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -50,9 +48,7 @@ app.use('/', index);
 app.use('/api', tasks);
 
 app.get('/api', function (req, res) {
-    /!*res.sendFile(path.join(__dirname+'/client/dist','index.html'))*!/
-   /!* res.sendFile(path.join(__dirname+'/public/dist/','index.html'))*!/
-    res.send('Hello');
+    res.sendFile(path.join(__dirname+'/client/dist','index.html'))
 });
 
 /!*app.use(function(req, res, next) {
@@ -63,52 +59,41 @@ app.get('/api', function (req, res) {
 
 
 
-/!*app.listen(3000, function () {
+app.listen(3000, function () {
     console.log('Example listening on port 3000!');
-});*!/
-app.listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
-
 });
 
 module.exports = app;
-
-
-/*
-
+*/
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname+'/public/dist'));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/dist/','index.html'));
-});
-
-app.listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
-
-});*/
-
-var express = require('express');
-var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
-app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname+'/public/dist/'));
-var bodyParser = require('body-parser');
+
+app.use('/',express.static(__dirname+'/client/dist'));
 // Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-/*app.use('/', index);
-app.use('/api', tasks);*/
+app.use('/', index);
+app.use('/api', tasks);
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/dist/index.html'));
-     /*res.send('Hi!');*/
-
+    res.send("Hi")
 });
-app.listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
 
+/*app.use(function(req, res, next) {
+ var err = new Error('Not Found');
+ err.status = 404;
+ next(err);
+ });*/
+
+
+
+app.listen(3000, function () {
+    console.log('Example listening on port 3000!');
 });
+
 module.exports = app;
