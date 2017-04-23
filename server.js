d6ec1c6efec52f0a10a4e733a0d6fc9e3e7f25cb
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-23 12:35:29 +0300

    init 38

--- server.js
+++ server.js
@@ -24,12 +24,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
 app.use('/api', tasks);
-
+app.get('/api', function (req, res) {
+    res.sendFile(path.join(__dirname+'/client/dist','index.html'))
+});
 app.listen(port, function(){
     console.log('Server started on port '+port);
 });
 
-
+module.exports = app;
 
 /*
 var express = require('express');
