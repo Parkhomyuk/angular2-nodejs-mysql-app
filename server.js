45cff083d0100074f0be37f75297586239754b25
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-23 10:33:32 +0300

    init 25

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.listen(3000, function () {
 
 module.exports = app;
 */
-var express = require('express');
+/*var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -87,15 +87,35 @@ app.get('/api', function (req, res) {
 
 });
 
-/*app.use(function(req, res, next) {
- var err = new Error('Not Found');
- err.status = 404;
- next(err);
- });*/
+
 
 
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
-module.exports = app;
+module.exports = app;*/
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public/dist'));
+app.set('views', __dirname + '/public/dist/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+app.get('*', function(req, res){
+    res.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+});
