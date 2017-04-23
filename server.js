5f383afd1e7e73320ea41baae2ab015769d12eaa
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-23 10:44:26 +0300

    init 26

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.listen(app.get('port'), function() {
 module.exports = app;*/
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -118,4 +118,7 @@ app.get('/', function(req, res){
 });
 
 app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
+
+module.exports = app;
