7f858df97596aa5e01541529533b803f7b1592a6
Alexey Parkhomyuk <parkhomyukalex@gmail.com>
2017-04-23 11:21:26 +0300

    init 33

--- server.js
+++ server.js
@@ -99,8 +99,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
-/*app.use('/', index);
-app.use('/api', tasks);*/
+app.use('/', index);
+/*app.use('/api', tasks);*/
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public/dist'));
 app.set('views', __dirname + '/public/dist/views');
